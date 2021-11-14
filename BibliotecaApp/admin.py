51f80e493abc56268529d9ea679d4da8ab52da79
gorka-larrea-montoya <gorkalarreamontoya@gmail.com>
2021-11-14 10:47:49 +0100

    Commit 02: Admin

--- BibliotecaApp/admin.py
+++ BibliotecaApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Libro, Escritor, Editorial, RelacionBiblioteca
+admin.site.register(Libro)
+admin.site.register(Escritor)
+admin.site.register(Editorial)
+admin.site.register(RelacionBiblioteca)
