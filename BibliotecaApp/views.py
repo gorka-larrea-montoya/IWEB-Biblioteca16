c4abcba990ad9e9526666c4985410dd60293fa3a
gorka-larrea-montoya <gorkalarreamontoya@gmail.com>
2021-11-14 10:55:02 +0100

    Commit 03 Vistas

--- BibliotecaApp/views.py
+++ BibliotecaApp/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, ,get_object_or_404,get_list_or_404
+from django.http import HttpResponse, Http404
+from .models import Libro, Escritor, Editorial, RelacionBiblioteca
 # Create your views here.
 
-
+#devuelve el listado de empresas
 def index(request):
-    return HttpResponse("Hello, World , Biblioteca Edition")
+    #return HttpResponse("Hello, World , Biblioteca Edition")
+    escritores = get_list_or_404(Escritor.objects.order_by("nombre"))
+    output = ', '.join([d.nombre for d in escritores])
+    return HttpResponse(output)
